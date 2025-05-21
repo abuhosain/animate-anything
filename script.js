675b32f4e9da7cbc0e8d1b053c5a3f9566d77ea9
Hosain <cryptichasan925@gmail.com>
2025-05-21 23:51:58 +0600

    locomotive js done

--- script.js
+++ script.js
@@ -1,45 +1,55 @@
 
-gsap.from("#page1 #bux", {
-  scale: 0,
-  duration: 1,
-  rotate: 360,
-  delay: 1,
+// locomotive scroll
+var main = document.querySelector("[data-scroll-container]");
+const scroll = new LocomotiveScroll({
+  el : main,
+  smooth : true,
+  lerp: 0.01
 })
 
 
 
-gsap.from("#page2 #bux", {
-  scale: 0,
-  duration: 1,
-  rotate: 360,
+
+// gspa scroll trigger
+
+// gsap.from("#page1 #bux", {
+//   scale: 0,
+//   duration: 1,
+//   rotate: 360,
+//   delay: 1,
+// })
+
+
+
+// gsap.from("#page2 #bux", {
+//   scale: 0,
+//   duration: 1,
+//   rotate: 360,
  
-  scrollTrigger: {
-    trigger : "#page2 #bux",
-    scroller: 'body',
-    markers: true,
-    start: "top 70%",
-    end : "top 30%",
-    scrub: 2,
-  },
-})
+//   scrollTrigger: {
+//     trigger : "#page2 #bux",
+//     scroller: 'body',
+//     markers: true,
+//     start: "top 70%",
+//     end : "top 30%",
+//     scrub: 2,
+//   },
+// })
 
 
-gsap.to("#page3 #bux", {
-  x : 600,
-  duration: 2,
-  scrollTrigger: {
-    trigger: "#page3 #bux",
-    scroller: 'body',
-    markers: true,
-    start: "top 60%",
-    end : "top 30%",
-    scrub: 2,
-    pin : true
-  }
-})
-
-
-
+// gsap.to("#page3 #bux", {
+//   x : 600,
+//   duration: 2,
+//   scrollTrigger: {
+//     trigger: "#page3 #bux",
+//     scroller: 'body',
+//     markers: true,
+//     start: "top 60%",
+//     end : "top 30%",
+//     scrub: 2,
+//     pin : true
+//   }
+// })
 
 
 
@@ -49,55 +59,58 @@ gsap.to("#page3 #bux", {
 
 
 
-gsap.from("#box", {
-  x: 1300,
-  duration: 2,
-  delay: 1,
-  backgroundColor: "green",
-  rotate: 360,
-  borderRadius: "50%",
-  scale: 0.5,
-});
+// gsap animaton
 
-gsap.to("#circle", {
-  x: 1300,
-  borderRadius: "10px",
-  duration: 2,
-  delay: 1,
-  rotate: 360,
-  y: 100,
-  backgroundColor: "red",
-  repeat: -1,
-  yoyo: true,
-});
 
-gsap.from("h1", {
-  y: 100,
-  duration: 2,
-  delay: 1,
-  opacity: 0,
-  scale: 0.5,
-  stagger: 1,
-});
+// gsap.from("#box", {
+//   x: 1300,
+//   duration: 2,
+//   delay: 1,
+//   backgroundColor: "green",
+//   rotate: 360,
+//   borderRadius: "50%",
+//   scale: 0.5,
+// });
 
-var tl = gsap.timeline();
+// gsap.to("#circle", {
+//   x: 1300,
+//   borderRadius: "10px",
+//   duration: 2,
+//   delay: 1,
+//   rotate: 360,
+//   y: 100,
+//   backgroundColor: "red",
+//   repeat: -1,
+//   yoyo: true,
+// });
 
-tl.to("#box1", {
-  x: 1500,
-  duration: 2,
-  delay: 1,
-});
+// gsap.from("h1", {
+//   y: 100,
+//   duration: 2,
+//   delay: 1,
+//   opacity: 0,
+//   scale: 0.5,
+//   stagger: 1,
+// });
 
-tl.to("#box2", {
-  x: 1500,
-  duration: 2,
-  rotate: 360,
+// var tl = gsap.timeline();
+
+// tl.to("#box1", {
+//   x: 1500,
+//   duration: 2,
+//   delay: 1,
+// });
+
+// tl.to("#box2", {
+//   x: 1500,
+//   duration: 2,
+//   rotate: 360,
    
-});
+// });
 
  
-tl.to("#box3", {
-  x: 1500,
-  duration: 2,
-  rotate: -360,
-});
+// tl.to("#box3", {
+//   x: 1500,
+//   duration: 2,
+//   rotate: -360,
+// });
